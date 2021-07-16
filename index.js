6c158e546eb37e734fffca6c514fb06816fe5b1d
Tino Bam Bino <alexanderthegrape19@gmail.com>
2021-07-15 20:44:57 -0500

    added questions

--- index.js
+++ index.js
@@ -20,7 +20,40 @@ const questions = [{
     },
     {
         type: 'input',
-        message: 'What can you do with your project?'
+        message: 'What can you do with your project?',
+        name: 'use',
+    },
+    {
+        type: 'list',
+        message: 'What license are you using?',
+        name: 'license',
+        choices: [
+            'none',
+            'apache License 2.0',
+            'GNU General Public License V3.0',
+            'MIT License',
+            'BSD 3-Clause'
+        ]
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidlines?',
+        name: 'help',
+    },
+    {
+        type: 'input',
+        message: 'Test Instructions?',
+        name: 'help',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your github username',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email address',
+        name: 'Email',
     }
 ];
 
